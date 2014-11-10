7df6bd8fd42a6a291d1f9bb78f44fc871edc0271
Appolloyon <cklinger@ualberta.ca>
2014-11-10 09:16:27 -0700

    rblast by Q or DB, with timestamp and tabular output, matches pattern for forward blastp scripts

--- rblastpg3.sh
+++ rblastpg3.sh
@@ -13,7 +13,8 @@
 # Concerning the remainder of this file,
 # Author: Jed Barlow
 # Author: Lael Barlow
-# Last Modified: July 25, 2012
+# Updates by: Christen Klinger
+# Last Modified: November 10, 2014
 
 
 # Info helpful for debugging
@@ -21,9 +22,10 @@ echo "Hostname is " $HOSTNAME
 pwd
 date
 
-QUERY_SUB_DIR=/home/bioinfor1/cklinger/BLAST_SEARCHES/rblastpqueries
-DATABASE_SUB_DIR=/home/bioinfor1/cklinger/BLAST_SEARCHES/rblastpdatabase
-BLAST_OPTIONS="-word_size 3 -gapopen 11 -gapextend 1 -evalue 1"
+QUERY_SUB_DIR=/home/cklinger/BLAST_SEARCHES/rblastpqueries
+DATABASE_SUB_DIR=/home/cklinger/BLAST_SEARCHES/rblastpdatabase
+BLAST_OPTIONS="-word_size 3 -gapopen 11 -gapextend 1 -evalue 0.1"
+now=$(date +"%Y%m%d")
 
 # Automatically find query files to use
 QUERYFILES=`ls $QUERY_SUB_DIR/*.fa`
@@ -42,14 +44,14 @@ for database in $DATAFILES; do
     db_short_name=$(basename "$database")
     db_short_name="${db_short_name%.*}"
 
-    mkdir -p /home/bioinfor1/cklinger/"rblastp_$db_short_name"
+    mkdir -p /home/cklinger/"rblastp_$q_short_name"
 
     for query in $QUERYFILES; do
         q_short_name=$(basename "$query")
         q_short_name="${q_short_name%.*}"
 
         ##send BLAST command to Cluster nodes
-        blastp -query "$query" -db "$database" -out "rblastp_$db_short_name/${q_short_name}_${db_short_name}.outfile.txt" $BLAST_OPTIONS
+        blastp -query "$query" -db "$database" -out "rblastp_$q_short_name/${q_short_name}_${db_short_namie}_${now}.outfile.txt" -outfmt "6 qseqid sseqid evalue" $BLAST_OPTIONS
     done
 done
 
